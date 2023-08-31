12b9fc243aad5242d311498ac7b8e20b552ac491
KURUMBU <kimariroy@gmail.com>
2023-08-31 18:45:12 +0300

    It will add dart file

---
+++ 1st.dart
@@ -0,0 +1,6 @@
+void main() {
+    var name = "Gladys Maina"; //string
+    var description = " is a student at PLP Africa Compassion Kenya. Cohort II"; //string
+    var phone = +254728569302; //number (no using of quotation marks)
+    print(name + description);
+}
